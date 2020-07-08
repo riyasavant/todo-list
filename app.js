6857caed4a0a8a32e1aeff9e0deb141c148d0cff
riyasavant <rieee2110@gmail.com>
2020-07-08 22:04:24 +0530

    Adding comments

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
+//require all the external modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+//create an array in the global scope to hold the items added or deleted.
 const items = ["Coding"];
-const titleName = "TO-DO LIST";
 
+//to tell express to make use of the static files in the public directory.
 app.use(express.static(__dirname+'/public'));
+
+//to parse the request
 app.use(bodyParser.urlencoded({extended: true}));
+
+//view engine ejs used for templating.
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render("todo", {title: titleName, todoItems: items});
+
+    //to get the weekday, month and day in either month or numeric format.
+    const event = new Date();
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
+
+    res.render("todo", {title: event.toLocaleDateString('en-GB', options), todoItems: items});
 });
+
 
 app.post('/', function(req, res) {
     
+    //to manage the inputs on add or delete buttons.
     if(req.body.button === 'pressed') {
         var getItem = req.body.item;
         items.push(getItem);
     } else if(req.body.buttonDelete === 'pressed') {
         items.pop();
     }
+
+    //riderection instead of render is used so that the same page is rendered again through the / route with dynamic changes.
     res.redirect('/');
 })
 
